83dbd8ca9966e0c67812528ddabcfe0ac5d7c3b7
zippn <nguyenhuuvu@gmail.com>
2017-11-30 16:50:37 -0500

    no message

--- js/pagination.js
+++ js/pagination.js
@@ -1,84 +1,45 @@
+
+
+
+function showPage(page) {
+
+    let startIndex = page*10-10;//where to start
+
+    //reset displayed student
+    $('.student-item').removeClass('active-student');
+    $('.pagination li a').removeClass('active');
+
+    $('.pagination li').eq(page-1).find('a').addClass('active');//set active page
+
+    for(let x=startIndex; x<startIndex+10; x+=1){
+        $('.student-item').eq(x).addClass('active-student');//set student to display
+       // console.log(x);
+    }
+
+}
+function appendPageLinks(studentList) {
+
 //Storing student list(s)
-var oldList = $('.student-list');
-var allStudents = oldList.children();
-var newArrayofStudentList = [];
+    let allStudentsSize = studentList.children().length;
 
-//define number of lists to make
-const newListSize = Math.ceil(allStudents.length/10);
+//define number of pages to make
+    let newListSize = Math.ceil(allStudentsSize/10);
 
 
 
 
+    let paginationHtml = `<div class="pagination"><ul>`;//open pagination
 
-
-function parseList() {
-
-    //track student
-    let currentStudent = 0;
-
-    console.log(allStudents);
-
-    for (let x = 0; x < newListSize; x+=1){
-        let studentList = [];//clear temp student list
-
-        //traverse 10 students
-        for (let y = 0; y < 10; y+=1){
-                if (allStudents[currentStudent]) {
-                    studentList.push(allStudents[currentStudent]);
-                    currentStudent+=1;
-                }
-
-        }
-        newArrayofStudentList.push(studentList);//new size 10 student list list
-//        console.log(studentList);
-
-
-    }
-}
-
-function createHtmlLists() {
-
-    let listsHtml = '';//store html for all lists
-
-    for (let x = 0; x < newArrayofStudentList.length; x += 1) {
-
-        let currentListHtml = `<ul id="list-${x+1}" class='student-list'>`;//initiate single list of 10 max students
-
-        for (let y = 0; y < 10; y+=1){
-            if (newArrayofStudentList[x][y]) {
-                currentListHtml += `<li class="student-item cf">${newArrayofStudentList[x][y].innerHTML}</li>`;
-            }
-
-        }
-
-        currentListHtml += `</ul>`;//close list
-        listsHtml +=currentListHtml;//add to combine list html
-
-
-    }
-    return listsHtml;
-}
-
-function createPagination() {
-
-    let paginationHtml = `<div class="pagination"><ul>`;
-
-    for (let x = 0; x < newArrayofStudentList.length; x += 1) {
-        paginationHtml += `<li><a href="#">${x+1}</a></li>`;
+    for (let x = 0; x < newListSize; x += 1) {
+        paginationHtml += `<li><a href="#" onclick="showPage(${x+1})">${x+1}</a></li>`;//call showPage when clicked
 
     }
     paginationHtml +=`</ul></div>`;
 
-    return paginationHtml;
+    $('.page').append(paginationHtml);
 }
-//parse the default list
-parseList();
-//attach new lists
-$('.page').append(createHtmlLists());
-$('.page').append(createPagination());
+//attach pagination
+appendPageLinks($('.student-list'));
+//set active
+showPage(1);
 
-
-//remove default list
-$('.student-list:first').remove();
-console.log(newArrayofStudentList);
-console.log(createHtmlLists());
